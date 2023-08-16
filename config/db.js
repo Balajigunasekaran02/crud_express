0645854a926aacaf856fdf6c8747ce2481f3705f
Balajigunasekaran02 <balajigunasekaran02@gmail.com>
2023-08-16 18:04:02 +0500

    async connection update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDb = async () => {
-  mongoose
+  await mongoose
     .connect(process.env.MONGO_URI)
     .then((conn) => {
       console.log(`DB connected Successfully: ${conn.connection.host}`);
